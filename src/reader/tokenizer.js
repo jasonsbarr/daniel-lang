d3a0c57b4ff73a992321075d8ff5b67caa8bafc9
Jason Barr <jason.barr@metronetinc.com>
2022-03-15 17:05:41 -0500

    Implement token class

--- src/reader/tokenizer.js
+++ src/reader/tokenizer.js
@@ -3,3 +3,24 @@ class LexerError extends Error {
     super(`Invalid token ${char} at (${line}:${col})`);
   }
 }
+
+/**
+ * A Token represents a single lexeme
+ */
+class Token {
+  constructor(type, name, text, line, col, pos) {
+    this.type = type;
+    this.name = name;
+    this.text = text;
+    this.line = line;
+    this.col = col;
+    this.pos = pos;
+  }
+
+  toString() {
+    return `Token(type=${this.type}, value=${this.text})`;
+  }
+}
+
+const token = (type, name, text, line, col, pos) =>
+  new Token(type, name, text, line, col, pos);
