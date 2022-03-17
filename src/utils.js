9329e3efaa677ea476d2a626a40cf02ca56ca1bc
Jason Barr <jason.barr@metronetinc.com>
2022-03-16 20:46:45 -0500

    Wrap fileURLToPath

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
-import { pathToFileURL } from "url";
+import { pathToFileURL, fileURLToPath } from "url";
 
 export const getFileURL = (path) => pathToFileURL(path).href;
+
+export const getURLFromPath = (url) => fileURLToPath(url);
