bb31f6b07cadae8a66e81cfa9f428183a5e7f71c
Jason Barr <jason.barr@metronetinc.com>
2022-03-17 16:41:05 -0500

    Add stdin to repl filename for parsing input

--- src/repl.js
+++ src/repl.js
@@ -2,7 +2,7 @@ import { input, print } from "../lib/js/io.js";
 import { read } from "./reader/reader.js";
 import { evalInput } from "./eval.js";
 
-const REP = (input) => print(evalInput(read(input), {}));
+const REP = (input) => print(evalInput(read(input, "<stdin>"), {}));
 
 while (true) {
   try {
