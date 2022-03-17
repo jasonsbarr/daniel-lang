7e2cd9c07411818be9c1940e64075d3fbe40fe5e
Jason Barr <jason.barr@metronetinc.com>
2022-03-17 16:41:15 -0500

    Add defer util

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,6 @@ import { pathToFileURL, fileURLToPath } from "url";
 export const getFileURL = (path) => pathToFileURL(path).href;
 
 export const dirname = (url) => dn(fileURLToPath(url));
+
+export const defer = (fn) =>
+  typeof "setImmediate" !== "undefined" ? setImmediate(fn) : setTimeout(fn, 0);
