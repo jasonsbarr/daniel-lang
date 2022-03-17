386d602c44f5e5f191486509d0cd30e453fa8709
Jason Barr <jason.barr@metronetinc.com>
2022-03-17 16:48:30 -0500

    Create Daniel functions with makeFunction

--- src/runtime.js
+++ src/runtime.js
@@ -1,1 +1,18 @@
-const makeFunction = (func, name, arity) => {};
+import { curry } from "@jasonsbarr/functional-core";
+
+/**
+ * Convert a JS function into a Daniel function object
+ *
+ * Daniel functions are autocurried
+ * @param {Function} func
+ * @param {String} name
+ * @param {Number} arity
+ */
+export const makeFunction = (func, name, arity) => {
+  name = name ?? (func.name || "<lambda>");
+  arity = arity ?? func.length;
+  func = curry(func);
+  func.name = name;
+  func.arity = arity;
+  return func;
+};
