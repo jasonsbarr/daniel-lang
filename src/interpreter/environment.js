6b8fd7de1c46a0e8ab57464a2729e54e9d83ca2a
Jason Barr <jason.barr@metronetinc.com>
2022-03-18 11:48:37 -0500

    Define Environment methods extend, get, lookup, and set

--- src/interpreter/environment.js
+++ src/interpreter/environment.js
@@ -13,7 +13,7 @@ class Environment {
    * @param {String} name Name for the new env
    */
   extend(name) {
-    return createEnv(this, { name });
+    return createEnv(this, { module: this.module, name });
   }
 
   /**
@@ -21,20 +21,37 @@ class Environment {
    * @param {Symbol} name Binding name to look up
    * @returns {Any}
    */
-  get(name) {}
+  get(name) {
+    const scope = this.lookup(name);
+    return scope.namespace[name];
+  }
 
   /**
    * Check if {name} exists in the current env or one of its parents
    * @param {Symbol} name
    */
-  lookup(name) {}
+  lookup(name) {
+    let scope = this;
+
+    while (scope) {
+      if (scope.namespace[name] !== undefined) {
+        return scope;
+      }
+
+      scope = scope.parent;
+    }
+
+    throw new RefError(name);
+  }
 
   /**
    * Bind a value to a name in the current environment
    * @param {Symbol} name
    * @param {Any} value
    */
-  set(name, value) {}
+  set(name, value) {
+    this.namespace[name] = value;
+  }
 
   toString() {
     return `Environment(module=${this.module}.${this.name})`;
