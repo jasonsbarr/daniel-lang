e0aa26ae489a606cb320b7885862f77a0b66c12a
Jason Barr <jason.barr@metronetinc.com>
2022-03-18 11:24:04 -0500

    Stub out class for Environment and ReferenceError

---
+++ src/interpreter/environment.js
@@ -0,0 +1,61 @@
+const { RuntimeError } = require("../../lib/js/error");
+
+class ReferenceError extends RuntimeError {
+  /**
+   * Error for when name is accessed without being initialized in the current scope
+   * @param {Symbol} name
+   * @param {String} msg
+   */
+  constructor(name, msg) {}
+}
+
+class Environment {
+  constructor(parent, module, name) {
+    this.parent = parent;
+    this.module = module;
+    this.name = name;
+    this.namespace = Object.create(null);
+  }
+
+  /**
+   * Create a new env with the current one as its parent
+   * @param {String} name Name for the new env
+   */
+  extend(name) {
+    return createEnv(this, { name });
+  }
+
+  /**
+   * Get the value for {name} from the current env or one of its parents
+   * @param {Symbol} name Binding name to look up
+   * @returns {Any}
+   */
+  get(name) {}
+
+  /**
+   * Check if {name} exists in the current env or one of its parents
+   * @param {Symbol} name
+   */
+  lookup(name) {}
+
+  /**
+   * Bind a value to a name in the current environment
+   * @param {Symbol} name
+   * @param {Any} value
+   */
+  set(name, value) {}
+
+  toString() {
+    return `Environment(module=${this.module}.${this.name})`;
+  }
+}
+
+const createEnv = (
+  parent = null,
+  { module = "<main>", name = "global" } = {}
+) => new Environment(parent, module, name);
+
+module.exports = {
+  Environment,
+  createEnv,
+};
