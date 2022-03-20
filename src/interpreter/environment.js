6fee9307ea6c0e30ecb0128f6a04be9f31a119b7
Jason Barr <jason.barr@metronetinc.com>
2022-03-19 20:16:26 -0500

    Add environment object

---
+++ src/interpreter/environment.js
@@ -0,0 +1,67 @@
+import { RefError } from "../../lib/js/error.js";
+
+export class Environment {
+  constructor(parent, module, name) {
+    this.parent = parent;
+    this.module = module;
+    this.name = name;
+    this.namespace = Object.create(null);
+    this.children = [];
+  }
+
+  /**
+   * Create a new env with the current one as its parent
+   * @param {String} name Name for the new env
+   */
+  extend(name) {
+    const env = createEnv(this, { module: this.module, name });
+    this.children.push(env);
+    return env;
+  }
+
+  /**
+   * Get the value for {name} from the current env or one of its parents
+   * @param {String} name Binding name to look up
+   * @returns {Any}
+   */
+  get(name) {
+    const scope = this.lookup(name);
+    return scope.namespace[name];
+  }
+
+  /**
+   * Check if {name} exists in the current env or one of its parents
+   * @param {String} name
+   */
+  lookup(name) {
+    let scope = this;
+
+    while (scope) {
+      if (scope.namespace[name] !== undefined) {
+        return scope;
+      }
+
+      scope = scope.parent;
+    }
+
+    throw new RefError(name);
+  }
+
+  /**
+   * Bind a value to a name in the current environment
+   * @param {String} name
+   * @param {Any} value
+   */
+  set(name, value) {
+    this.namespace[name] = value;
+  }
+
+  toString() {
+    return `Environment(module=${this.module}-${this.name})`;
+  }
+}
+
+export const createEnv = (
+  parent = null,
+  { module = "<global>", name = "global" } = {}
+) => new Environment(parent, module, name);
