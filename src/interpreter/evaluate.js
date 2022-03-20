3f5b4c1a6b1086175041e1a8b015a2367f258202
Jason Barr <jason.barr@metronetinc.com>
2022-03-20 02:03:20 -0500

    Add evalSet function

--- src/interpreter/evaluate.js
+++ src/interpreter/evaluate.js
@@ -49,6 +49,9 @@ const evalList = (ast, env) => {
     case "define":
       return evalDefine(ast, env);
 
+    case "set!":
+      return evalSet(ast, env);
+
     default:
       return evalCall(ast, env);
   }
@@ -159,6 +162,23 @@ const evalFor = (ast, env) => {
  * @param {Environment} env
  */
 const evalDefine = (ast, env) => {
+  if (ast.length !== 3) {
+    throw new RuntimeError("Define must have exactly 2 subexpressions");
+  }
+
+  const id = ast[1];
+  const expr = ast[2];
+  const name = id.value;
+
+  env.set(name, evaluate(expr, env));
+};
+
+/**
+ * Define a new binding in the current environment
+ * @param {Array} ast
+ * @param {Environment} env
+ */
+const evalSet = (ast, env) => {
   if (ast.length !== 3) {
     throw new RuntimeError("Define must have exactly 2 subexpressions");
   }
