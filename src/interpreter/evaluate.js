7ba824fdc414265e0bd79c5aec5acfa0ac24406a
Jason Barr <jason.barr@metronetinc.com>
2022-03-19 20:23:48 -0500

    Add looking up vars to evaluate, call expressions, and simple global environment for repl

--- src/interpreter/evaluate.js
+++ src/interpreter/evaluate.js
@@ -3,6 +3,10 @@ export const evaluate = (ast, env = {}) => {
     return evalList(ast, env);
   }
 
+  if (ast.type === "Symbol") {
+    return evalSymbol(ast, env);
+  }
+
   return ast.value;
 };
 
@@ -18,7 +22,7 @@ const evalList = (ast, env) => {
       return evalBlock(ast.slice(1), env);
 
     default:
-      return null;
+      return evalCall(ast, env);
   }
 };
 
@@ -30,4 +34,33 @@ const evalBlock = (ast, env) => {
   }
 
   return value;
+};
+
+/**
+ *
+ * @param {Object[]} ast
+ * @param {Environment} env
+ * @returns
+ */
+const evalCall = (ast, env) => {
+  if (ast[0] === undefined) {
+    return null;
+  }
+
+  const fst = evaluate(ast[0], env);
+
+  if (typeof fst !== "function") {
+    throw new RuntimeError(
+      `Expected a function that can be applied to arguments; got ${typeof fst}`
+    );
+  }
+
+  const args = ast.slice(1).map((arg) => evaluate(arg, env));
+
+  return fst.call(null, ...args);
+};
+
+const evalSymbol = (ast, env) => {
+  const val = env.get(ast.value);
+  return val;
 };
--- src/repl.js
+++ src/repl.js
@@ -1,8 +1,23 @@
 import { input, println } from "../lib/js/io.js";
 import { read } from "./reader/reader.js";
 import { evaluate } from "./interpreter/evaluate.js";
+import { createEnv } from "./interpreter/environment.js";
 
-const REP = (input) => println(evaluate(read(input, "<stdin>"), {}));
+const env = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+  "%": (a, b) => a % b,
+};
+
+const globalEnv = createEnv();
+
+for (let [k, v] of Object.entries(env)) {
+  globalEnv.set(k, v);
+}
+
+const REP = (input) => println(evaluate(read(input, "<stdin>"), globalEnv));
 
 while (true) {
   try {
