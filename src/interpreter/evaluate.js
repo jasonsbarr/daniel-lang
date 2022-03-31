cad1145d6e4d7f9585fc1ec178a7fd720de889f8
Jason Barr <jason.barr@metronetinc.com>
2022-03-30 20:42:28 -0500

    Fix bug causing syntax tree not to be evaluated if 1st item in ast is an array

--- src/interpreter/evaluate.js
+++ src/interpreter/evaluate.js
@@ -46,7 +46,12 @@ export const evaluate = async (ast, env, module = "<main>") => {
     // On the off chance someone defines an object with a type attr of, e.g. "String",
     // and a non-empty syntax attr, this will fail. The likelihood of that seems remote.
     // An empty list will still fall through and be returned as null from evalList
-    if (fst && !primitives.includes(fst.type) && !fst.syntax) {
+    if (
+      fst &&
+      !primitives.includes(fst.type) &&
+      !fst.syntax &&
+      !Array.isArray(fst)
+    ) {
       return ast;
     }
 
