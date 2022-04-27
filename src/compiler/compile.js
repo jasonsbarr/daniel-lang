d28151d32c3ab955729de5f0110531c398bfd44a
Jason Barr <jason.barr@metronetinc.com>
2022-04-27 13:37:01 -0500

    Use generic visitor for now

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -1,5 +1,10 @@
 import { read } from "../reader/reader.js";
 import { makeSexprs } from "./s-expr.js";
 import { emit } from "./emit.js";
+import { Visitor } from "./visitor.js";
 
-export const compile = (code) => emit(makeSexprs(read(code)));
+class GenericVisitor extends Visitor {}
+
+let visitor = new GenericVisitor();
+
+export const compile = (code) => emit(makeSexprs(visitor.visit(read(code))));
