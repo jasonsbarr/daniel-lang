0155da74ad46ed4fbe2c5c07988706fee66c0f29
Jason Barr <jason.barr@metronetinc.com>
2022-04-27 14:39:27 -0500

    Create symbol table class

--- src/compiler/symbol-table.js
+++ src/compiler/symbol-table.js
@@ -1,1 +1,33 @@
-class SymbolTable {}
+class SymbolTable {
+  constructor(parent = null) {
+    this.parent = parent;
+    this.namespace = Object.create(null);
+  }
+
+  get(sym) {
+    const scope = this.lookup(sym);
+    return scope.namespace[sym];
+  }
+
+  lookup(sym) {
+    if (sym in this.namespace) {
+      return this;
+    }
+
+    let scope = this.parent;
+
+    while (scope !== null) {
+      if (sym in scope.namespace) {
+        return scope;
+      }
+
+      scope = scope.parent;
+    }
+
+    throw new Error(`Symbol ${Symbol.keyFor(sym)} not found in scope`);
+  }
+
+  set(sym, val) {
+    this.namespace[sym] = val;
+  }
+}
