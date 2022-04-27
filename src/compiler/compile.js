f04bb884a032e9d9cde60f327262c2f97900ee25
Jason Barr <jason.barr@metronetinc.com>
2022-04-27 10:59:07 -0500

    Don't forget to convert to s-expressions in compile

--- src/compiler/compile.js
+++ src/compiler/compile.js
@@ -1,4 +1,5 @@
 import { read } from "../reader/reader.js";
+import { makeSexprs } from "./s-expr.js";
 import { emit } from "./emit.js";
 
-export const compile = (code) => emit(read(code));
+export const compile = (code) => emit(makeSexprs(read(code)));
